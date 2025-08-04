cdb2f6fc7a3742894f44493c5bb3e44da1b83ef0
Radhi Hudijan <radhi.hudijan@accenture.com>
2025-08-04 16:33:18 +0200

    Update environment variable to use workspace context in global_variable.tf

--- global_variable.tf
+++ global_variable.tf
@@ -2,9 +2,8 @@ locals {
   default_tags = {
     Project     = "eks-terraform"
     Owner       = "radhi.hudijan"
-    Environment = "Development"
+    Environment = "${terraform.workspace}-environment"
     ManagedBy   = "Terraform"
-    workspace   = terraform.workspace
   }
 
   aws_region = "eu-central-1"
